4366fc085140da5a186df51c92fff84db15493e7
elisa6454 <79989274+elisa6454@users.noreply.github.com>
2022-06-20 01:33:00 +0900

    .

--- js/weather.js
+++ js/weather.js
@@ -4,8 +4,6 @@ const API_KEY = "1a1812dc216aca495399707012b11793";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
   fetch(url)
@@ -15,7 +13,6 @@ function onGeoOk(position) {
       weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
-
 function onGeoError() {
   alert("Can't find you. No weather for you.");
 }
